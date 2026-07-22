7a5dd5b383f5dbf85c48cc9a85c921168717e63e
ismayildasdemirli27 <dasdemirliismayil40@gmail.com>
2026-07-22 05:31:49 +0000

    Fix promises order: uploadPhoto first, signUpUser second

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -3,8 +3,8 @@ import uploadPhoto from './5-photo-reject.js';
 
 export default function handleProfileSignup(firstName, lastName, filename) {
   return Promise.allSettled([
-    signUpUser(firstName, lastName),
     uploadPhoto(filename),
+    signUpUser(firstName, lastName),
   ]).then((results) => results.map((result) => ({
     status: result.status,
     value: result.status === 'fulfilled' ? result.value : result.reason,
