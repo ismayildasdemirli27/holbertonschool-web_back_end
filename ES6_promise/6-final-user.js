bd5efcaec6365ef62fc7173bce78218f5cfc8c37
ismayildasdemirli27 <dasdemirliismayil40@gmail.com>
2026-07-22 05:25:11 +0000

    Fix promise order in handleProfileSignup

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,13 +2,11 @@ import signUpUser from './4-user-promise.js';
 import uploadPhoto from './5-photo-reject.js';
 
 export default async function handleProfileSignup(firstName, lastName, filename) {
-  const results = await Promise.allSettled([
-    signUpUser(firstName, lastName),
+  return Promise.allSettled([
     uploadPhoto(filename),
-  ]);
-
-  return results.map((result) => ({
-    status: result.status,
-    value: result.status === 'fulfilled' ? result.value : result.reason,
-  }));
+    signUpUser(firstName, lastName),
+  ]).then((values) => values.map((v) => ({
+    status: v.status,
+    value: v.status === 'fulfilled' ? v.value : v.reason,
+  })));
 }
