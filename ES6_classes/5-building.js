b5448b1b28aa727b0da862e90f41ba46f761045d
ismayildasdemirli27 <dasdemirliismayil40@gmail.com>
2026-07-22 04:10:55 +0000

    5. Building abstract class

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,19 @@
+export default class Building {
+  constructor(sqft) {
+    this.sqft = sqft;
+    if (this.constructor !== Building && this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
+       throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+     if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(value) {
+    this._sqft = value;
+  }
+}
