4fc83db199e36d43597cdffad000e2c38a0f6843
ismayildasdemirli27 <dasdemirliismayil40@gmail.com>
2026-07-22 05:27:21 +0000

    Fix order and implementation in handleProfileSignup

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,12 +1,12 @@
 import signUpUser from './4-user-promise.js';
 import uploadPhoto from './5-photo-reject.js';
 
-export default async function handleProfileSignup(firstName, lastName, filename) {
+export default function handleProfileSignup(firstName, lastName, filename) {
   return Promise.allSettled([
-    uploadPhoto(filename),
     signUpUser(firstName, lastName),
-  ]).then((values) => values.map((v) => ({
-    status: v.status,
-    value: v.status === 'fulfilled' ? v.value : v.reason,
+    uploadPhoto(filename),
+  ]).then((results) => results.map((result) => ({
+    status: result.status,
+    value: result.status === 'fulfilled' ? result.value : result.reason,
   })));
 }
