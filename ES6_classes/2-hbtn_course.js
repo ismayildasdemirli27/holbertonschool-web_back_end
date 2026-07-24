310ed78e034fe04043736ee81a4500602498ba95
ismayildasdemirli27 <dasdemirliismayil40@gmail.com>
2026-07-24 21:43:16 +0000

    Implement HolbertonCourse class with getters, setters and type checking for Check 2

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,40 +1,50 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    this.name = name;
-    this.length = length;
-    this.students = students;
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    if (!Array.isArray(students)) {
+      throw new TypeError('Students must be an array');
+    }
+
+    this._name = name;
+    this._length = length;
+    this._students = students;
   }
 
   get name() {
     return this._name;
   }
 
-  set name(value) {
-    if (typeof value !== 'string') {
+  set name(newVal) {
+    if (typeof newVal !== 'string') {
       throw new TypeError('Name must be a string');
     }
-    this._name = value;
+    this._name = newVal;
   }
 
   get length() {
     return this._length;
   }
 
-  set length(value) {
-    if (typeof value !== 'number') {
+  set length(newVal) {
+    if (typeof newVal !== 'number') {
       throw new TypeError('Length must be a number');
     }
-    this._length = value;
+    this._length = newVal;
   }
 
   get students() {
     return this._students;
   }
 
-  set students(value) {
-    if (!Array.isArray(value) || !value.every((item) => typeof item === 'string')) {
-      throw new TypeError('Students must be an array of strings');
+  set students(newVal) {
+    if (!Array.isArray(newVal)) {
+      throw new TypeError('Students must be an array');
     }
-    this._students = value;
+    this._students = newVal;
   }
 }
