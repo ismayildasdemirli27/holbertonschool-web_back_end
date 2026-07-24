91b7272403c941ffc02d8583c0691c53b0abab1b
ismayildasdemirli27 <dasdemirliismayil40@gmail.com>
2026-07-24 21:45:50 +0000

    Implement Pricing class with getters, setters, displayFullPrice and static convertPrice method for Check 4

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -2,40 +2,31 @@ import Currency from './3-currency.js';
 
 export default class Pricing {
   constructor(amount, currency) {
-    this.amount = amount;
-    this.currency = currency;
+    this._amount = amount;
+    this._currency = currency;
   }
 
   get amount() {
     return this._amount;
   }
 
-  set amount(value) {
-    if (typeof value !== 'number') {
-      throw new TypeError('Amount must be a number');
-    }
-    this._amount = value;
+  set amount(newAmount) {
+    this._amount = newAmount;
   }
 
   get currency() {
     return this._currency;
   }
 
-  set currency(value) {
-    if (!(value instanceof Currency)) {
-      throw new TypeError('Currency must be an instance of Currency');
-    }
-    this._currency = value;
+  set currency(newCurrency) {
+    this._currency = newCurrency;
   }
 
   displayFullPrice() {
-    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
   }
 
   static convertPrice(amount, conversionRate) {
-    if (typeof amount !== 'number' || typeof conversionRate !== 'number') {
-      throw new TypeError('Amount and conversionRate must be numbers');
-    }
     return amount * conversionRate;
   }
 }
