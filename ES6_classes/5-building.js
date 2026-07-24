48143dd54e7902099485ae9ed64579e25facd0bc
ismayildasdemirli27 <dasdemirliismayil40@gmail.com>
2026-07-24 21:48:38 +0000

    Implement Building abstract class with sqft and evacuationWarningMessage check for Check 5

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,19 +1,14 @@
 export default class Building {
   constructor(sqft) {
-    this.sqft = sqft;
-    if (this.constructor !== Building && this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
-       throw new Error('Class extending Building must override evacuationWarningMessage');
-    }
-     if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
+    this._sqft = sqft;
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error('Class extending Building must override evacuationWarningMessage');
+      }
     }
   }
 
   get sqft() {
     return this._sqft;
-  }
-
-  set sqft(value) {
-    this._sqft = value;
   }
 }
