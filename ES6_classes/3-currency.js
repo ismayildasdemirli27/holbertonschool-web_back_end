6498ec65aeb308a18a7796fff85eee92213a9e1e
ismayildasdemirli27 <dasdemirliismayil40@gmail.com>
2026-07-24 21:44:23 +0000

    Implement Currency class with getters, setters and displayFullCurrency method for Check 3

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,26 +1,26 @@
 export default class Currency {
   constructor(code, name) {
-    this.code = code;
-    this.name = name;
+    this._code = code;
+    this._name = name;
   }
 
   get code() {
     return this._code;
   }
 
-  set code(value) {
-    this._code = value;
+  set code(newCode) {
+    this._code = newCode;
   }
 
   get name() {
     return this._name;
   }
 
-  set name(value) {
-    this._name = value;
+  set name(newName) {
+    this._name = newName;
   }
 
   displayFullCurrency() {
-    return `${this.name} (${this.code})`;
+    return `${this._name} (${this._code})`;
   }
 }
