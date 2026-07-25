1d594c58c0577bfcdcf2e05aaf326174a834f2fb
Ismayil <dasdemirliismayil40@gmail.com>
2026-07-25 22:42:33 +0400

    Update 1-stdin.js

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,7 +1,8 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('readable', () => {
   const chunk = process.stdin.read();
+
   if (chunk !== null) {
     process.stdout.write(`Your name is: ${chunk}`);
   }
