2cf409df1817758fef197fe4f195f182f5114b6b
Ismayil <dasdemirliismayil40@gmail.com>
2026-07-25 17:19:11 +0400

    Update 10-update_topics.py

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
-""" Module that changes all topics of a school document based on the name """
+"""Module for updating school topics."""
 
 
 def update_topics(mongo_collection, name, topics):
-    """ Changes all topics of a school document """
+    """Change all topics of a school document."""
+
     mongo_collection.update_many(
         {"name": name},
         {"$set": {"topics": topics}}
