995c2ab239f38ee7637115b748b6621c06708a61
ismayildasdemirli27 <dasdemirliismayil40@gmail.com>
2026-07-25 17:11:39 +0400

    8. List all documents in Python

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-"""Module for listing documents"""
+""" Module that lists all documents in a MongoDB collection """
 
 
 def list_all(mongo_collection):
-    """Return all documents in a collection"""
+    """ Lists all documents in a collection """
+    if mongo_collection is None:
+        return []
     return list(mongo_collection.find())
-  
