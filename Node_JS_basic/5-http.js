1335161723d56a780df451870f7a659ce5aa5471
ismayildasdemirli27 <dasdemirliismayil40@gmail.com>
2026-07-25 19:01:49 +0000

    5. Create a more complex HTTP server using Node's HTTP module

---
+++ Node_JS_basic/5-http.js
@@ -0,0 +1,69 @@
+const http = require('http');
+const fs = require('fs');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+      
+      const lines = data.replace(/\r/g, '').split('\n').filter((line) => line.trim() !== '');
+      if (lines.length <= 1) {
+        resolve('Number of students: 0');
+        return;
+      }
+      
+      lines.shift(); // Başlıqları çıxarırıq
+      let result = `Number of students: ${lines.length}\n`;
+      const fields = {};
+      
+      for (const line of lines) {
+        const studentData = line.split(',');
+        if (studentData.length >= 4) {
+          const firstName = studentData[0].trim();
+          const field = studentData[3].trim();
+          if (!fields[field]) {
+            fields[field] = [];
+          }
+          fields[field].push(firstName);
+        }
+      }
+      
+      const entries = Object.entries(fields);
+      for (let i = 0; i < entries.length; i++) {
+        const [field, names] = entries[i];
+        result += `Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`;
+        if (i < entries.length - 1) {
+          result += '\n';
+        }
+      }
+      resolve(result);
+    });
+  });
+}
+
+const app = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+
+  if (req.url === '/') {
+    res.statusCode = 200;
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    res.statusCode = 200;
+    res.write('This is the list of our students\n');
+    // process.argv[2] terminaldan verilən database.csv faylının adını götürür
+    countStudents(process.argv[2])
+      .then((data) => {
+        res.end(data);
+      })
+      .catch((err) => {
+        res.end(err.message);
+      });
+  }
+});
+
+app.listen(1245);
+
+module.exports = app;
