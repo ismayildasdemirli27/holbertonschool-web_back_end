e2021eccdea4bb8a55b9d2602ad8f8920494327f
Ismayil <dasdemirliismayil40@gmail.com>
2026-07-27 10:48:03 +0400

    Refactor AppController to use default export

--- Node_JS_basic/full_server/controllers/AppController.js
+++ Node_JS_basic/full_server/controllers/AppController.js
@@ -1,7 +1,5 @@
-class AppController {
+export default class AppController {
   static getHomepage(request, response) {
     response.status(200).send('Hello Holberton School!');
   }
 }
-
-export default AppController;
