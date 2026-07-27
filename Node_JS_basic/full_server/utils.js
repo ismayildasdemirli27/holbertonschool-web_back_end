71f7feed4b220d8b84112bb37228239ef48e5a25
Ismayil <dasdemirliismayil40@gmail.com>
2026-07-27 10:49:41 +0400

    Update utils.js

--- Node_JS_basic/full_server/utils.js
+++ Node_JS_basic/full_server/utils.js
@@ -1,33 +1,28 @@
-import fs from 'fs';
+import { promises as fs } from 'fs';
 
-export const readDatabase = (filePath) => new Promise((resolve, reject) => {
-  if (!filePath) {
-    reject(new Error('Cannot load the database'));
-    return;
-  }
-  fs.readFile(filePath, 'utf8', (err, data) => {
-    if (err) {
-      reject(new Error('Cannot load the database'));
-      return;
-    }
-    const lines = data.replace(/\r/g, '').split('\n').filter((line) => line.trim() !== '');
-    if (lines.length <= 1) {
-      reject(new Error('Cannot load the database'));
-      return;
-    }
-    lines.shift();
-    const fields = {};
-    for (const line of lines) {
-      const student = line.split(',');
-      if (student.length >= 4) {
-        const firstName = student[0].trim();
-        const field = student[3].trim();
-        if (!fields[field]) fields[field] = [];
-        fields[field].push(firstName);
+const readDatabase = async (filePath) => {
+  const data = await fs.readFile(filePath, 'utf-8');
+
+  const lines = data.split('\n');
+  const rows = lines.slice(1);
+  const result = {};
+
+  for (const row of rows) {
+    if (row.trim() !== '') {
+      const parts = row.split(',');
+
+      const firstName = parts[0];
+      const field = parts[3];
+
+      if (!result[field]) {
+        result[field] = [];
       }
+
+      result[field].push(firstName);
     }
-    resolve(fields);
-  });
-});
+  }
+
+  return result;
+};
 
 export default readDatabase;
